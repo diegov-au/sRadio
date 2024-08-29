59d5c05d3343fa8967098e19e399799a534c6988
Diego V <diegov@diegov.com>
2024-08-29 14:21:58 -0700

    Add files via upload

---
+++ DataModel/MediaItemClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace sRadio.DataModel
+{
+    public class MediaItemClass
+    {
+        public string MediaName { get; set; }
+        public string MediaDescription { get; set; }
+        public MediaTypes MediaType { get; set; } // eg. StreamingRadio, LocalFile, PodCast
+        public string MediaUri { get; set; }
+        public string MediaLogoUrl { get; set; }
+    }
+
+    public enum MediaTypes
+    {
+        StreamingRadio,
+        LocalMedia,
+        RemoteMedia,
+        PodCast
+    }
+}
---
+++ DataModel/MediaMetadataStateClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+//used to store state of currently playing media
+
+namespace sRadio.DataModel
+{
+    public class MediaMetadataStateClass
+    {
+        public string MediaName { get; set; }
+        public string MediaTitle{ get; set; }
+        public string MediaArtist { get; set; }
+        public string MediaAlbum { get; set; }
+        public string MediaBitrate { get; set; }
+        public string MediaSamplerate { get; set; }
+        public string MediaCodec { get; set; }
+        public Bitmap MediaArt { get; set; }
+   
+    }
+}
